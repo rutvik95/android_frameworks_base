3698052e69b3214cb70875bea3a5ceb265d18acc
cristianomatos <cristianobmatos@gmail.com>
2014-01-22 10:05:02 -0200

    Custom clear recents button (1/2)     - Option to show/hide     - Option to choose which corner to place (top left, top right, bottom left, bottom right)
    REMEMBER TO ADD THIS AFTER THE BEFORE COMMIT
    Thanks omnirom team
    Original commit: https://github.com/omnirom/android_frameworks_base/commit/dc9ddf62c4a0b1710ad3ce4bdf19ce5dcf63094c
    
    Change-Id: I9c15fee94de38cd94a8d685b76a43af58c48bd5e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    	packages/SystemUI/res/layout-land/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout/status_bar_recent_panel.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3446,6 +3446,18 @@ public final class Settings {
         public static final String LOCKSCREEN_LONG_MENU_ACTION = "lockscreen_long_menu_action";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_RECENTS_BUTTON = "clear_recents_button";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String CLEAR_RECENTS_BUTTON_LOCATION = "clear_recents_button_location";
+
+        /**
          * Expanded desktop on/off state
 <<<<<<< HEAD
          * @hide
