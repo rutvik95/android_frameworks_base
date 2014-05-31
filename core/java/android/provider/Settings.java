be5bbd5d7b8aa99976ce5b5ce1737e58b24861f7
Schubi <schubi@erlangen.ccc.de>
2014-05-31 13:09:36 +0200

    Slim Recents: Option to show topmost task [1/2]
    Change-Id: Ib39d9b87b0fd30b4d34d4292af39e891038f337a
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3939,6 +3939,13 @@ public final class Settings {
         public static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
 
         /**
+         * Recent panel: Show topmost task
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+
+        /**
          * Size of recent panel view in percent (default = 100).
          * @hide
          */
