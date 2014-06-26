17bbe440b382b0dc626e60e4cad9e94c11951fb1
Daniel Koman <dankoman30@gmail.com>
2014-06-26 02:53:30 +0200

    FB: Slimrecents background color && transparency [2/2]
    PS: now setting sticks through reboots and TRDS state
            changes. also, add reset to default color option.
    
    PS: when resetting to default, use TRDS (png) resources
            instead of custom color
    
    PS: okay, should be good to go now  :D
    
    Change-Id: Icf0259ce45d416364d6e7a7a8c2038f6c4c0cbde

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4383,6 +4383,13 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
 
         /**
+         * Recent panel background color
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
