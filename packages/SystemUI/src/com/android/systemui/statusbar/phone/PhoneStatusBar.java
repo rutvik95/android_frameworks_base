ef8cc91dc218650279af82747973478fd9f53a14
Clark Scheff <clark@cyngn.com>
2014-07-08 10:21:39 -0700

    HeadsUp: Only remove heads up if attached to window
    Attempting to remove mHeadsUpNoficiationView from the window manager
    when it is not currently attached will throw an IllegalArgumentException
    so we check if it is attached and if so remove it, otherwise carry on.
    
    As well remove possible left children in statusbarview
    
    Change-Id: Ida916eba496c4183e47288ef24a8a93c46a7eddf
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3820,11 +3820,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void setHeadsUpVisibility(boolean vis) {
         if (DEBUG) Log.v(TAG, (vis ? "showing" : "hiding") + " heads up window");
-        mHeadsUpNotificationView.setVisibility(vis ? View.VISIBLE : View.GONE);
-        if (!vis) {
-            if (DEBUG) Log.d(TAG, "setting heads up entry to null");
-            mInterruptingNotificationEntry = null;
-            mHeadsUpPackageName = null;
+        if (mHeadsUpNotificationView != null && mHeadsUpNotificationView.isAttachedToWindow()) {
+            mHeadsUpNotificationView.setVisibility(vis ? View.VISIBLE : View.GONE);
+            if (!vis) {
+                if (DEBUG) Log.d(TAG, "setting heads up entry to null");
+                mInterruptingNotificationEntry = null;
+                mHeadsUpPackageName = null;
+            }
         }
     }
 
@@ -3870,7 +3872,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void recreateStatusBar() {
         mRecreating = true;
+        
         removeHeadsUpView();
+
         mStatusBarContainer.removeAllViews();
         mStatusBarContainer.clearDisappearingChildren();
 
