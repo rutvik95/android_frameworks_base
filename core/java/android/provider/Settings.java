3a34612276c4ed89f4bef9509faf6d3accaa3516
lion0738 <lion0738@naver.com>
2014-07-31 20:06:54 -0300

    Launch Heads Up in floating window [1/2]     AICP'fied.     To make floating window to work for Hover and Heads Up, notificationhelper     interface is modified to include a third parameter to indicate whether it's     called from hover or not.     To make Heads_Up floating notification work separate from Hover,     a third parameter is added to the notificationhelper to indicate whether     we are called from Hover.java. This changes the call.     PS2-3: Fix authorship     PS5: separate hover change.     PS6: change commit message.     PS7: Merge with Hover change, fix setting preference and code cleanup.          To make settings work, don't use Boolean with CM CheckBox preference     PS8: autorship
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationHelper.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4328,6 +4328,13 @@ public final class Settings {
         public static final String HEADS_UP_SHOW_UPDATE = "heads_up_show_update";
 
         /**
+         * Heads Up in Floating Window
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_FLOATING_WINDOW = "heads_up_floating_window";
+
+        /**
 		 * User favorite tasks for recent panel.
          * @hide
          */
