6b85423beacf89f24809a624d2cefca6d73a7591
Lars Greiss <kufikugel@googlemail.com>
2014-07-31 17:44:49 -0300

    fb: Show heads up at the bottom of the screen (1/2) Due of user requests and indeed especially on hw key devices for a better workflow we add an option to show heads up notifications on the bottom instead of the top.
    Animation we leave as is due that if it slide from the bottom into the screen it happens
    to often that the user accidentaly interacts with it. If it comes from the top of the
    container we add, the user has enough time to realize it. Sometimes a ms can make a big difference.
    
    As well this commit fixes the wrong gap on the top if user boots up directly
    into immersive mode.
    
    PS:
    - if IME keyboard is showing show the heads up on the top to do not disturb the typing.
    PS:
    - NPE if updatePosition is called to early from the system.
    
    Conflicts:
    	packages/SystemUI/res/values/axxion_dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4292,6 +4292,13 @@ public final class Settings {
         public static final String STATUS_BAR_NOTIFICATION_SWIPE_FLOATING = "status_bar_notification_swipe_floating";
          
         /**
+         * Whether heads up notification is shown on the bottom of the screen (default = disabled)
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_GRAVITY_BOTTOM = "heads_up_gravity_bottom";
+
+        /**
          * Whether heads up notification is expanded by default (default = disabled)
          *
          * @hide
