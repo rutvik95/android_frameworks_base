3d9fe0a2e4fc22ba99bd3574714ffecf32be4d67
siracuervo <fl.siracusa@outlook.com.ar>
2014-08-24 12:44:01 -0300

    Final fixes

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3658,6 +3658,13 @@ public final class Settings {
         public static final String CALL_UI_AS_HEADS_UP = "call_ui_as_heads_up";
 
         /**
+         * Whether incomming call UI stays in background
+         *
+         * @hide
+         */
+        public static final String CALL_UI_IN_BACKGROUND = "call_ui_in_background";
+
+        /**
          * Whether to use the custom quick unlock screen control
          * @hide
          */
@@ -3670,34 +3677,34 @@ public final class Settings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
-	/**
-	* If On-The-Go should be displayed at the power menu.
-	*
-	* @hide
-	*/
+	    /**
+	     * If On-The-Go should be displayed at the power menu.
+	     *
+	     * @hide
+	     */
         public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
 
-	/**
-	* The alpha value of the On-The-Go overlay.
-	*
-	* @hide
-	*/
+	    /**
+	     * The alpha value of the On-The-Go overlay.
+	     *
+	     * @hide
+	     */
         public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
 
-	/**
-	* The camera instance to use.
-	* 0 = Rear Camera
-	* 1 = Front Camera
-	*
-	* @hide
-	*/
+	    /**
+	     * The camera instance to use.
+	     * 0 = Rear Camera
+	     * 1 = Front Camera
+	     *
+	     * @hide
+	     */
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
-	/**
-	* Whether the service should restart itself or not.
-	*
-	* @hide
-	*/
+	    /**
+	     * Whether the service should restart itself or not.
+	     *
+	     * @hide
+	     */
         public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
 
         /**
