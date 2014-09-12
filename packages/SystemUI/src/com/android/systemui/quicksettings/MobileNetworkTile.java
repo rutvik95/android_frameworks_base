ba244011bd3f0d85971541cdba7f73785878d73e
thatkawaiiguy <ivanthegreat15@gmail.com>
2014-09-11 22:19:01 -0400

    FIXXX

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTile.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
